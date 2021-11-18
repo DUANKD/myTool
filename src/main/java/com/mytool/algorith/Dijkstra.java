855e77e223a5312f3201114491abd028bc15355d
duankd <duankd@corp.21cn.com>
2021-11-18 23:51:58 +0800

    feat:算法-Dijkstra算法问题

--- src/main/java/com/mytool/algorith/Dijkstra.java
+++ src/main/java/com/mytool/algorith/Dijkstra.java
@@ -10,38 +10,39 @@ import java.util.Queue;
  * @date 2021-11-18 22:55
  */
 public class Dijkstra {
-    static class node {
+    static class Node {
         int x; //节点编号
         int length;//长度
 
-        public node(int x, int length) {
+        public Node(int x, int length) {
             this.x = x;
             this.length = length;
         }
     }
 
-    public static void main(String[] args) {
 
-        int[][] map = new int[6][6];//记录权值，顺便记录链接情况，可以考虑附加邻接表
-        initMap(map);//初始化
+    public static int[] stackDijkstra(int[][] map, int start) {
         boolean bool[] = new boolean[6];//判断是否已经确定
-        int len[] = new int[6];//长度
-        for (int i = 0; i < 6; i++) {
+        int len[] = new int[map.length];//长度
+        for (int i = 0; i < map.length; i++) {
             len[i] = Integer.MAX_VALUE;
         }
-        Queue<node> q1 = new PriorityQueue<node>(com);
+        StringBuilder stringBuilder = new StringBuilder();
+        Queue<Node> q1 = new PriorityQueue<Node>(com);
         len[0] = 0;//从0这个点开始
-        q1.add(new node(0, 0));
+        q1.add(new Node(0, 0));
+        stringBuilder.append("路径：");
         int count = 0;//计算执行了几次dijkstra
         while (!q1.isEmpty()) {
-            node t1 = q1.poll();
+            Node t1 = q1.poll();
             int index = t1.x;//节点编号
             int length = t1.length;//节点当前点距离
             bool[index] = true;//抛出的点确定
+            stringBuilder.append(t1.x).append("->");
             count++;//其实执行了6次就可以确定就不需要继续执行了  这句可有可无，有了减少计算次数
             for (int i = 0; i < map[index].length; i++) {
                 if (map[index][i] > 0 && !bool[i]) {
-                    node node = new node(i, length + map[index][i]);
+                    Node node = new Node(i, length + map[index][i]);
                     if (len[i] > node.length)//需要更新节点的时候更新节点并加入队列
                     {
                         len[i] = node.length;
@@ -50,35 +51,75 @@ public class Dijkstra {
                 }
             }
         }
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < map.length; i++) {
             System.out.println(len[i]);
         }
+        System.out.println(stringBuilder.toString());
+        return len;
     }
 
-    static Comparator<node> com = new Comparator<node>() {
+    public static int[] dijkstra(int[][] weight, int start) {
+        // 接受一个有向图的权重矩阵，和一个起点编号start（从0编号，顶点存在数组中）
+        // 返回一个int[] 数组，表示从start到它的最短路径长度
+        int n = weight.length; // 顶点个数
+        int[] shortPath = new int[n]; // 保存start到其他各点的最短路径
+        String[] path = new String[n]; // 保存start到其他各点最短路径的字符串表示
+        for (int i = 0; i < n; i++) {
+            path[i] = new String(start + "-->" + i + ":" + start);
+        }
+        int[] visited = new int[n]; // 标记当前该顶点的最短路径是否已经求出,1表示已求出
+
+        // 初始化，第一个顶点已经求出
+        shortPath[start] = 0;
+        visited[start] = 1;
+
+        for (int count = 1; count < n; count++) { // 要加入n-1个顶点
+            int k = -1; // 选出一个距离初始顶点start最近的未标记顶点
+            int dmin = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == 0 && weight[start][i] < dmin && weight[start][i] > 0) {
+                    //if (visited[i] == 0 && weight[start][i] < dmin ) {
+                    dmin = weight[start][i];
+                    k = i;
+                }
+            }
+
+            // 将新选出的顶点标记为已求出最短路径，且到start的最短路径就是dmin
+            /*if(dmin>0&&k!=-1){
+                shortPath[k] = dmin;
+            }*/
+            if (dmin > 0 && k != -1) {
+                shortPath[k] = dmin;
+                visited[k] = 1;
+                path[k] = path[k] + "-->" + k;
+                // 以k为中间点，修正从start到未访问各点的距离
+                for (int i = 0; i < n; i++) {
+                    //如果 '起始点到当前点距离' + '当前点到某点距离' < '起始点到某点距离', 则更新
+                    if (visited[i] == 0 && shortPath[k] + weight[k][i] < weight[start][i]) {
+                        weight[start][i] = shortPath[k] + weight[k][i];
+                        path[i] = path[k] + "-->" + i;
+                    }
+                }
+
+            }
+
+
+        }
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("从" + start + "出发到" + i + "的最短路径为：" + path[i] + ",长度为：" + shortPath[i]);
+        }
+        System.out.println("=====================================");
+        return shortPath;
+    }
+
+    static Comparator<Node> com = new Comparator<Node>() {
 
         @Override
-        public int compare(node o1, node o2) {
+        public int compare(Node o1, Node o2) {
             return o1.length - o2.length;
         }
     };
 
-    private static void initMap(int[][] map) {
-        map[0][1] = 2;
-        map[0][2] = 3;
-        map[0][3] = 6;
-        map[1][0] = 2;
-        map[1][4] = 4;
-        map[1][5] = 6;
-        map[2][0] = 3;
-        map[2][3] = 2;
-        map[3][0] = 6;
-        map[3][2] = 2;
-        map[3][4] = 1;
-        map[3][5] = 3;
-        map[4][1] = 4;
-        map[4][3] = 1;
-        map[5][1] = 6;
-        map[5][3] = 3;
-    }
+
 }
---
+++ src/test/java/com/mytool/algorith/DijkstraTest.java
@@ -0,0 +1,38 @@
+package com.mytool.algorith;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DijkstraTest {
+
+    @Test
+    void dijkstra() {
+        int[][] weight=initMap();
+        int[] result= Dijkstra.dijkstra(weight,0);
+        int[] result1= Dijkstra.stackDijkstra(weight,0);
+        System.out.println("result:"+result);
+        System.out.println("result1:"+result1);
+    }
+
+    private static int[][] initMap() {
+        int[][] map=new int[6][6];
+        map[0][1] = 2;
+        map[0][2] = 3;
+        map[0][3] = 6;
+        map[1][0] = 2;
+        map[1][4] = 4;
+        map[1][5] = 6;
+        map[2][0] = 3;
+        map[2][3] = 2;
+        map[3][0] = 6;
+        map[3][2] = 2;
+        map[3][4] = 1;
+        map[3][5] = 3;
+        map[4][1] = 4;
+        map[4][3] = 1;
+        map[5][1] = 6;
+        map[5][3] = 3;
+        return map;
+    }
+}
