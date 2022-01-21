3186cf8223a45477e8906251270ef75b7f104293
duankd <duankd@corp.21cn.com>
2022-01-21 11:47:06 +0800

    feat:24点

--- src/test/java/com/mytool/algorith/JudgePoint24Test.java
+++ src/test/java/com/mytool/algorith/JudgePoint24Test.java
@@ -6,7 +6,7 @@ class JudgePoint24Test {
 
     @Test
     void judgePoint24() {
-        int[] card = new int[]{1, 24, 25, 26};
+        int[] card = new int[]{10, 24, 22, 26};
         int target = 25;
         boolean result = JudgePoint24.judgePoint24(target, card);
         System.out.println(result);
